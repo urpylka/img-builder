07e63c35ad8e3eb0ae616c9f63327f73c42ab9d8
Artem Smirnov <urpylka@gmail.com>
2021-01-05 00:10:35 +0400

    Fix locale

--- assets/make-setup.sh
+++ assets/make-setup.sh
@@ -25,6 +25,30 @@ sed -i 's/#SystemMaxUse=/SystemMaxUse=200M/' /etc/systemd/journald.conf
 echo "> Changing default keyboard layout to US"
 sed -i 's/XKBLAYOUT="gb"/XKBLAYOUT="us"/g' /etc/default/keyboard
 
+# perl: warning: Setting locale failed.
+# perl: warning: Please check that your locale settings:
+# 	LANGUAGE = (unset),
+# 	LC_ALL = (unset),
+# 	LC_CTYPE = "UTF-8",
+# 	LANG = "en_GB.UTF-8"
+#     are supported and installed on your system.
+# perl: warning: Falling back to a fallback locale ("en_GB.UTF-8").
+# locale: Cannot set LC_CTYPE to default locale: No such file or directory
+# locale: Cannot set LC_ALL to default locale: No such file or directory
+
+# https://stackoverflow.com/questions/11300633/svn-cannot-set-lc-ctype-locale
+# https://rtfm.co.ua/linux-cannot-set-lc_ctype-to-default-locale-no-such-file-or-directory/
+# https://proadminz.ru/ispravlenie-oshibok-locale-cannot-set/
+
+# echo "> Setting LC_CTYPE and LC_ALL"
+# cat << EOF >> /home/pi/.bashrc
+# export LANG='C.UTF-8'
+# export LC_ALL='C.UTF-8'
+# EOF
+
+sed -i 's/en_GB.UTF-8 UTF-8/#en_GB.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+
 echo "> Setting a short aliases"
 cat << EOF >> /home/pi/.bashrc
 alias status='sudo systemctl status'
