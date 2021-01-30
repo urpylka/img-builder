f8bd91c548d72941c216cb627877b6ba5540ffcc
Artem Smirnov <urpylka@gmail.com>
2021-01-31 01:29:10 +0400

    Update make-init

--- assets/make-init.sh
+++ assets/make-init.sh
@@ -7,12 +7,12 @@ set -e # Exit immidiately on non-zero result
 [[ -z ${3} ]] && (echo "Wasn't set the original url"; exit 1)
 
 echo "> Putting image information"
-cat >> "/etc/theimage" << EOF
-image project: ${1}
-image version: ${2}
-image original: ${3%.*}
+cat >> "/boot/img-builder/theimage.conf" << EOF
+image_project: ${1}
+image_version: ${2}
+image_original: ${3%.*}
+image_interfaces: /boot/img-builder/interfaces-router.conf
 EOF
-cp /etc/theimage /boot/img-builder/theimage
 
 echo "> Writing magic script to /etc/rc.local"
 MAGIC_SCRIPT="sudo /root/make-once.sh; sudo sed -i '/sudo \\\/root\\\/make-once.sh/d' /etc/rc.local && sudo reboot"
