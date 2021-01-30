e39247d2506976268442bde86236462fcd64d25c
Artem Smirnov <urpylka@gmail.com>
2021-01-31 01:03:18 +0400

    Add file to /boot/img-builder

--- assets/make-init.sh
+++ assets/make-init.sh
@@ -7,11 +7,12 @@ set -e # Exit immidiately on non-zero result
 [[ -z ${3} ]] && (echo "Wasn't set the original url"; exit 1)
 
 echo "> Putting image information"
-cat >> "/etc/builder" << EOF
+cat >> "/etc/theimage" << EOF
 image project: ${1}
 image version: ${2}
 image original: ${3%.*}
 EOF
+cp /etc/theimage /boot/img-builder/theimage
 
 echo "> Writing magic script to /etc/rc.local"
 MAGIC_SCRIPT="sudo /root/make-once.sh; sudo sed -i '/sudo \\\/root\\\/make-once.sh/d' /etc/rc.local && sudo reboot"
