fb76aead09153751604876cddb6881ec392b185a
Artem Smirnov <urpylka@gmail.com>
2021-03-07 19:28:04 +0400

    Cleanup

--- assets/make-init.sh
+++ assets/make-init.sh
@@ -12,7 +12,6 @@ cat >> "/boot/img-builder/theimage.conf" << EOF
 image_project=${1}
 image_version=${2}
 image_original=${3}
-image_interfaces=/boot/img-builder/interfaces-router.conf
 image_id=TEMP
 EOF
 
